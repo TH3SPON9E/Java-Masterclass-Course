08a8332601da8b3f9e00b18c67e44899ae549123
TH3SPON9E <th3spon9e@gmail.com>
2020-02-13 13:18:02 +0000

    Updated the challenge AbstractClassesChallenge

---
+++ Section9/AbstractClassesChallenge/src/com/nmbrookes/MyLinkedList.java
@@ -0,0 +1,72 @@
+package com.nmbrookes;
+
+public class MyLinkedList implements NodeList {
+
+    private ListItem root = null;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        if(this.root == null) {
+            // The list was empty, so this item becomes the head of the list
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while(currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if(comparison < 0) {
+                // New item is greater, move right if possible
+                if(currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                }
+                else {
+                    // There is no next, so insert at the end of the list
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            }
+            else if(comparison > 0) {
+                // New item is less, insert before
+                if(currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setNext(newItem);
+                }
+                else {
+                    // There node without a previous is the root
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            }
+            else {
+                // Equal
+                System.out.println(newItem.getValue() + " is already present, not added.");
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
