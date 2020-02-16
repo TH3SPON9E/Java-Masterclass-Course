cf4919577e5374f23a3a73017a9af4067bbd180b
TH3SPON9E <th3spon9e@gmail.com>
2020-02-16 22:48:08 +0000

    Added and completed WinDemo

---
+++ Section11/WinDemo/src/com/nmbrookes/Main.java
@@ -0,0 +1,9 @@
+package com.nmbrookes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyWindow myWindow = new MyWindow("Complete Java");
+	    myWindow.setVisible(true);
+    }
+}
