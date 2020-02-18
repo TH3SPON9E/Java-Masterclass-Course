69e3cb803cb1457270f87df9e5c9bd3e06c80bb2
TH3SPON9E <th3spon9e@gmail.com>
2020-02-18 21:45:19 +0000

    Completed the exercise Scopes

--- Section11/Scope/src/com/nmbrookes/Main.java
+++ Section11/Scope/src/com/nmbrookes/Main.java
@@ -1,13 +1,28 @@
 package com.nmbrookes;
 
+// Scope ensures the local variable with a narrow scope is used before accessing global variable
+// Java checks the current block to see if the variable is declares and using this one
+// If not Java checks the next enclosing block and so on until finding the variable it needs.
+
 public class Main {
 
     public static void main(String[] args) {
-        String privateVar = "this is private to main";
+        String varFour = "this is private to main";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar = " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        scopeInstance.useInner();
 
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        System.out.println("varThree is not accessible here " + innerClass.varThree);
+
+//        System.out.println("scopeInstance varOne = " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//
+//        scopeInstance.timesTwo();
+//
+//        System.out.println("**************************");
+//
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
     }
 }
