cdc1aa1f7170cd1fd702aee85f4995c0c400d027
TH3SPON9E <th3spon9e@gmail.com>
2020-02-19 19:53:56 +0000

    Added exercise Collections

---
+++ Section12/Collections/src/com/nmbrookes/Main.java
@@ -0,0 +1,33 @@
+package com.nmbrookes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Odean", 8, 12);
+//        theatre.getSeats();
+
+        // Reserve seat
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay.");
+        }
+        else {
+            System.out.println("Sorry, seat is taken.");
+        }
+
+        // Try reserving a reserved seat
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay.");
+        }
+        else {
+            System.out.println("Sorry, seat is taken.");
+        }
+
+        // Try reserving a seat that doesn't exist
+        if(theatre.reserveSeat("Z11")) {
+            System.out.println("Please pay.");
+        }
+        else {
+            System.out.println("Sorry, seat is taken.");
+        }
+    }
+}
