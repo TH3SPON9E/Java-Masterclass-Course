fa88cb8f21ff27439826b2c6b6651b5a7165382c
TH3SPON9E <th3spon9e@gmail.com>
2020-02-26 17:19:01 +0000

    Completed the challenge sortedCollections

--- Section12/sortedCollections/src/com/nmbrookes/Basket.java
+++ Section12/sortedCollections/src/com/nmbrookes/Basket.java
@@ -1,7 +1,6 @@
 package com.nmbrookes;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -21,6 +20,26 @@ public class Basket {
             return inBasket;
         }
         return 0;
+    }
+
+    public int removeFromBasket(StockItem item, int quantity) {
+        if(item != null && quantity > 0) {
+            int inBasket = list.getOrDefault(item, 0);
+            int newQuantity = inBasket - quantity;
+            if(newQuantity > 0) {
+                list.put(item, newQuantity);
+                return quantity;
+            }
+            else if(newQuantity == 0) {
+                list.remove(item);
+                return quantity;
+            }
+        }
+        return 0;
+    }
+
+    public void clearBasket() {
+        this.list.clear();
     }
 
     public Map<StockItem, Integer> Items() {
--- Section12/sortedCollections/src/com/nmbrookes/Main.java
+++ Section12/sortedCollections/src/com/nmbrookes/Main.java
@@ -37,6 +37,7 @@ public class Main {
         }
 
         Basket matthewsBasket = new Basket("Matthew");
+
         sellItem(matthewsBasket, "Car", 1);
         System.out.println(matthewsBasket);
 
@@ -46,27 +47,50 @@ public class Main {
         if(sellItem(matthewsBasket, "Car", 1) != 1) {
             System.out.println("No more stock for car");
         }
-        System.out.println(matthewsBasket);
 
         sellItem(matthewsBasket, "Spanner", 1);
-        System.out.println(matthewsBasket);
-
         sellItem(matthewsBasket, "Juice", 4);
         sellItem(matthewsBasket, "Cup", 12);
         sellItem(matthewsBasket, "Bread", 1);
+
+        Basket tomsBasket = new Basket("Tom");
+        sellItem(tomsBasket, "Cup", 100);
+        sellItem(tomsBasket, "Juice", 5);
+        removeItem(tomsBasket, "Cup", 1);
+        removeItem(tomsBasket, "Cup", 3);
+        System.out.println(tomsBasket);
+
+        removeItem(matthewsBasket, "Car", 1);
+        removeItem(matthewsBasket, "Cup", 9);
+        removeItem(matthewsBasket, "Car", 1);
+        System.out.println("Cars removed: " + removeItem(matthewsBasket, "Car", 1)); // Should not remove anything
+        System.out.println(matthewsBasket);
+
+        // Remove all items from Matthews Basket
+        removeItem(matthewsBasket, "Bread", 1);
+        removeItem(matthewsBasket, "Juice", 4);
+        removeItem(matthewsBasket, "Cup", 3);
+        System.out.println(matthewsBasket);
+
+        System.out.println("Display stock before checkout");
+        System.out.println(stockList);
+        System.out.println(matthewsBasket);
+        System.out.println(tomsBasket);
+
+        System.out.println("Display stock after checkout");
+        System.out.println(tomsBasket);
+        System.out.println(stockList);
+        checkout(tomsBasket);
+        System.out.println(tomsBasket);
+        System.out.println(stockList);
+
+//        for(Map.Entry<String, Double> price: stockList.priceList().entrySet()) {
+//            System.out.println(price.getKey() + " costs: " + price.getValue());
+//        }
+        checkout(matthewsBasket);
         System.out.println(matthewsBasket);
 
         System.out.println(stockList);
-
-//        temp = new StockItem("pen", 12);
-//        stockList.Items().put(temp.getName(), temp);
-        stockList.Items().get("Car").adjustStock(2000);
-        stockList.get("Car").adjustStock(-1000);
-        System.out.println(stockList);
-
-        for(Map.Entry<String, Double> price: stockList.priceList().entrySet()) {
-            System.out.println(price.getKey() + " costs: " + price.getValue());
-        }
 
 
     }
@@ -78,10 +102,29 @@ public class Main {
             System.out.println("We don't sell " + item);
             return 0;
         }
-        if(stockList.sellStock(item, quantity) != 0) {
-            basket.addToBasket(stockItem, quantity);
-            return quantity;
+        if(stockList.reserveStock(item, quantity) != 0) {
+            return basket.addToBasket(stockItem, quantity);
         }
         return 0;
+    }
+
+    public static int removeItem(Basket basket, String item, int quantity) {
+        // Retrieve item from stockList
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(basket.removeFromBasket(stockItem, quantity) == quantity) {
+            return stockList.removeReservedStock(item, quantity);
+        }
+        return 0;
+    }
+
+    public static void checkout(Basket basket) {
+        for(Map.Entry<StockItem, Integer> item: basket.Items().entrySet()) {
+            stockList.sellStock(item.getKey().getName(), item.getValue());
+        }
+        basket.clearBasket();
     }
 }
